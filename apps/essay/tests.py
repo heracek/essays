af57e33e7a41169030b6b18faf0a9f31345670e3
Tomas Horacek <tomas.horacek@gmail.com>
2008-08-06 00:04:25 +0200

    Pridany fce pro parserovani textu.

---
+++ apps/essay/tests.py
@@ -0,0 +1,64 @@
+import unittest
+from utils import split_text_to_words, count_words_in_list_to_dict, split_text_to_words_dict_with_counts
+
+
+class UtilsSplitTextToWordsTestCase(unittest.TestCase):
+    def testSimpleText(self):
+        text = 'Simple text.'
+        
+        self.assertEqual(
+            split_text_to_words(text),
+            ['Simple', 'text']
+        )
+        
+    def testTwoSentences(self):
+        text = 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.\nDuis tortor eros. Duis tortor eros, adipiscing sed, scelerisque a, faucibus in, purus.'
+        
+        self.assertEqual(
+            split_text_to_words(text),
+            ['Lorem', 'ipsum', 'dolor', 'sit', 'amet', 'consectetuer', 'adipiscing', 'elit',
+             'Duis', 'tortor', 'eros', 'Duis', 'tortor', 'eros', 'adipiscing', 'sed', 'scelerisque',
+             'a', 'faucibus', 'in', 'purus']
+        )
+
+
+class UtilsCountWordsInListToDictTestCase(unittest.TestCase):
+    def testSimpleText(self):
+        words_list = ['Simple', 'text']
+        
+        self.assertEqual(
+            count_words_in_list_to_dict(words_list),
+            { 'simple': 1, 'text': 1 }
+        )
+        
+        
+    def testTwoSentences(self):
+        words_list = ['Lorem', 'ipsum', 'dolor', 'sit', 'amet', 'consectetuer', 'adipiscing', 'elit',
+         'Duis', 'tortor', 'eros', 'Duis', 'tortor', 'eros', 'adipiscing', 'sed', 'scelerisque',
+         'a', 'faucibus', 'in', 'purus']
+        
+        self.assertEqual(
+            count_words_in_list_to_dict(words_list),
+            { 'a': 1, 'ipsum': 1, 'sed': 1, 'tortor': 2, 'sit': 1, 'scelerisque': 1, 'in': 1, 'purus': 1,
+              'elit': 1, 'duis': 2, 'adipiscing': 2, 'dolor': 1, 'consectetuer': 1, 'lorem': 1, 'amet': 1,
+              'faucibus': 1, 'eros': 2 }
+        )
+
+class UtilsSplitTextToWordsDictWithCountsTestCase(unittest.TestCase):
+    def testSimpleText(self):
+        text = 'Simple text.'
+        
+        self.assertEqual(
+            split_text_to_words_dict_with_counts(text),
+            { 'simple': 1, 'text': 1 }
+        )
+    
+    def testTwoSentences(self):
+        text = 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.\nDuis tortor eros. Duis tortor eros, adipiscing sed, scelerisque a, faucibus in, purus.'
+        
+        self.assertEqual(
+            split_text_to_words_dict_with_counts(text),
+            { 'a': 1, 'ipsum': 1, 'sed': 1, 'tortor': 2, 'sit': 1, 'scelerisque': 1, 'in': 1, 'purus': 1,
+              'elit': 1, 'duis': 2, 'adipiscing': 2, 'dolor': 1, 'consectetuer': 1, 'lorem': 1, 'amet': 1,
+              'faucibus': 1, 'eros': 2 }
+        )
---
+++ apps/essay/utils.py
@@ -0,0 +1,28 @@
+
+def split_text_to_words(text):
+    words_list = []
+    last_aplha_chars = []
+    
+    for char in text:
+        if char.isalpha():
+            last_aplha_chars.append(char)
+        else:
+            if last_aplha_chars:
+                words_list.append(''.join(last_aplha_chars))
+                last_aplha_chars = []
+    return words_list
+
+def count_words_in_list_to_dict(words_list):
+    words_cout_dict = {}
+    for word in words_list:
+        word_lower = word.lower()
+        if word_lower in words_cout_dict:
+            words_cout_dict[word_lower] += 1
+        else:
+            words_cout_dict[word_lower] = 1
+    return words_cout_dict
+            
+
+def split_text_to_words_dict_with_counts(text):
+    words_list = split_text_to_words(text)
+    return count_words_in_list_to_dict(words_list)
