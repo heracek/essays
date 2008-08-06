0345b1f8ac4fc4444d6f0ce9787b155a99a80b82
Tomas Horacek <tomas.horacek@gmail.com>
2008-08-06 02:27:16 +0200

    Pridano metoda eseje pro zobrazeni textu jako automaticky hypertext.

--- apps/essay/admin.py
+++ apps/essay/admin.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 import models
 
 class EssayAdmin(admin.ModelAdmin):
-    list_display = list_display_links = search_fields = (
-        'title', 'slug', 'text', )
+    list_display = (
+        'title', 'slug', 'text', 'get_hypertexted_text')
+    list_display_links = search_fields = (
+        'title', 'slug', 'text')
     prepopulated_fields = { 'slug': ('title', )}
 admin.site.register(models.Essay, EssayAdmin)
 
--- apps/essay/models.py
+++ apps/essay/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from utils import split_text_to_words_dict_with_counts
+from StringIO import StringIO
 
 class Essay(models.Model):
     title = models.CharField(max_length=100)
@@ -28,6 +29,31 @@ class Essay(models.Model):
                 hyperword = HyperWord.objects.create(word=word, count=count)
             
             HyperWordsInEssays.objects.create(hyperword=hyperword, essay=self, count=count)
+    
+    def get_hypertexted_text(self):
+        hypertexted_text = StringIO()
+        last_aplha_chars_len = 0
+
+        for (i, char) in enumerate(self.text):
+            if char.isalpha():
+                last_aplha_chars_len += 1
+            else:
+                if last_aplha_chars_len:
+                    word = self.text[i-last_aplha_chars_len:i]
+                    
+                    try:
+                        hyperword = HyperWord.objects.get(word=word.lower())
+                        hypertexted_text.write('<a href="%s">%s</a>' % (hyperword.get_absolute_url(), word))
+                    except HyperWord.DoesNotExist:
+                        hypertexted_text.write(word)
+                    last_aplha_chars_len = 0
+                hypertexted_text.write(char)
+        if last_aplha_chars_len:
+            pass
+        
+        hypertexted_text.seek(0)
+        return hypertexted_text.read()
+    get_hypertexted_text.allow_tags = True
         
 
 class HyperWord(models.Model):
@@ -37,6 +63,9 @@ class HyperWord(models.Model):
     
     def __unicode__(self):
         return u'HyperWord: %s' % self.word
+    
+    def get_absolute_url(self):
+        return '/word/%s/' % self.word
     
 class HyperWordsInEssays(models.Model):
     hyperword = models.ForeignKey(HyperWord)
